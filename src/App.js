ab47c16e2fc0735918f1c958c94da0107d8ca2d8
Honeyfield <HoneyfieK@corning.com>
2020-06-03 22:57:41 -0400

    built most of the features

--- src/App.js
+++ src/App.js
@@ -1,30 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 
 // component imports
 import SearchForm from './components/SearchForm'
 import NavButtons from './components/NavButtons'
-import PhotoList from './components/PhotoList'
+import PhotoContainer from './components/PhotoContainer'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      {/* <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header> */}
 
-      <SearchForm />
+const APIKEY = process.env.REACT_APP_API_KEY || 'apikey'
+class App extends Component{
 
-      <NavButtons />
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+      loading: true
+    }
+  } 
 
-      <PhotoList />
+  componentDidMount() {
 
+    this.performSearch()
+  }
 
-    </div>
-  );
+  performSearch = (query = `field`) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${APIKEY}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then(response => {
+      this.setState({
+        photos: response.data.photos.photo,
+        loading: false
+      })
+      console.log(response)
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    })
+  }
+  // performSearch = (query = `yeehaw`) => {
+  //   axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=${APIKEY}`)
+  //   .then(response => {
+  //     this.setState({
+  //       gifs: response.data.data,
+  //       loading: false
+  //     })
+  //   })
+  //   .catch(error => {
+  //     console.log('Error fetching and parsing data', error);
+  //   })
+  // }
+
+  render(){
+    return (
+      <div className="container">
+  
+        <SearchForm onSearch={this.performSearch}/>
+  
+        <NavButtons />
+  
+        <PhotoContainer photos={this.state.photos}/>
+  
+      </div>
+    )
+  }
+  
 }
 
 export default App;
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,15 +1,32 @@
 import React from 'react'
 
 //import components
+import Photo from './Photo'
 import NoResults from './NoResults'
 
 
-const PhotoList = () => {
+const PhotoList = (props) => {
+    //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    const results = props.photos
+    console.log(props)
+    let photos
+    if(results.length > 0){
+      photos = results.map(foto =>  (
+                 <Photo 
+                    url={`https://farm${foto.farm}.staticflickr.com/${foto.server}/${foto.id}_${foto.secret}.jpg`} 
+                    key={foto.id} 
+                  /> 
+            )) 
+      
+    }
+    else{
+      photos = <NoResults />
+    }
     return(
         <div className="photo-container">
         <h2>Results</h2>
         <ul>
-          <li>
+          {/* <li>
             <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
           </li>
           <li>
@@ -20,7 +37,8 @@ const PhotoList = () => {
           </li>
           <li>
             <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-          </li>
+          </li> */}
+          {photos}
 
         </ul>
       </div>
