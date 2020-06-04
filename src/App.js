b5dcea33512fbb40e9efed4ede178ceabfe6a6e8
Honeyfield <HoneyfieK@corning.com>
2020-06-03 20:10:37 -0400

    basic layout to match expected layout completed, added favicon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+
+// component imports
+import SearchForm from './components/SearchForm'
+import NavButtons from './components/NavButtons'
+import PhotoList from './components/PhotoList'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      {/* <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      </header>
+      </header> */}
+
+      <SearchForm />
+
+      <NavButtons />
+
+      <PhotoList />
+
+
     </div>
   );
 }
---
+++ src/components/NoResults.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NoResults = () => {
+    return(
+
+          <li className="not-found">
+            <h3>No Results Found</h3>
+            <p>Your search did not return any results. Please try again.</p>
+          </li>
+
+    )
+}
+
+export default NoResults
